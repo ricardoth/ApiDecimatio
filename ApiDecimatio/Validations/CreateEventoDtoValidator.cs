9bb5dcf916596f6f6243250d9a3a50a7465c776e
ricardoth <ricardotilleriaochoa@gmail.com>
2025-08-05 14:36:16 -0400

    cambios de nombres y fix

--- ApiDecimatio/Validations/CreateEventoDtoValidator.cs
+++ ApiDecimatio/Validations/CreateEventoDtoValidator.cs
@@ -10,15 +10,16 @@
             RuleFor(x => x.Descripcion).NotEmpty().WithMessage("Descripcion es obligatorio.");
             RuleFor(x => x.Fecha).NotEmpty().WithMessage("Fecha es obligatorio.");
             RuleFor(x => x.ProductoraResponsable).NotEmpty().WithMessage("ProductoraResponsable es obligatorio.");
-            RuleFor(x => x.Banner).
-                 NotNull()
-                 .When(x => x.Banner.HasValue);
-            RuleFor(x => x.ContenidoBanner).
-                MinimumLength(3).MaximumLength(400)
+            RuleFor(x => x.Banner)
+                .NotNull()
+                .When(x => x.Banner);
+            RuleFor(x => x.ContenidoBanner)
+                .MinimumLength(3).MaximumLength(400)
                 .When(x => !string.IsNullOrEmpty(x.ContenidoBanner));
             RuleFor(x => x.Observacion).
                 MinimumLength(3).MaximumLength(2000)
                 .When(x => !string.IsNullOrEmpty(x.Observacion));
+
         }
     }
 }
--- Decimatio.Domain/DTOs/UpdateEventoDto.cs
+++ Decimatio.Domain/DTOs/UpdateEventoDto.cs
@@ -8,7 +8,6 @@
         public string? Descripcion { get; set; }
         public DateTime? Fecha { get; set; }
         public string? Flyer { get; set; }
-        public string? ContenidoFlyer { get; set; }
         public string? Base64ImagenFlyer { get; set; }
         public string? Observacion { get; set; }
         public string? ProductoraResponsable { get; set; }

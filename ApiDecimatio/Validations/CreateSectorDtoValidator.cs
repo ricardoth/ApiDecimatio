658fd709cd3d8141463086ec31a958976fc65cab
ricardoth <ricardotilleriaochoa@gmail.com>
2026-02-03 13:01:48 -0300

    fix

--- ApiDecimatio/Validations/CreateSectorDtoValidator.cs
+++ ApiDecimatio/Validations/CreateSectorDtoValidator.cs
@@ -9,9 +9,9 @@
             RuleFor(x => x.Precio).GreaterThan(0).WithMessage("El precio debe ser mayor a 0");
             RuleFor(x => x.Cargo).GreaterThan(0).WithMessage("El precio debe ser mayor a 0");
             RuleFor(x => x.Total).GreaterThan(0).WithMessage("El precio debe ser mayor a 0");
-            RuleFor(x => x.CapacidadActual).NotEmpty().WithMessage("La Capacidad Actual es obligatoria");
-            RuleFor(x => x.CapacidadDisponible).NotEmpty().WithMessage("La Capacidad Disponible es obligatoria");
-            RuleFor(x => x.CapacidadTotal).NotEmpty().WithMessage("La Capacidad Total es obligatoria");
+            //RuleFor(x => x.CapacidadActual).NotEmpty().WithMessage("La Capacidad Actual es obligatoria");
+            //RuleFor(x => x.CapacidadDisponible).NotEmpty().WithMessage("La Capacidad Disponible es obligatoria");
+            //RuleFor(x => x.CapacidadTotal).NotEmpty().WithMessage("La Capacidad Total es obligatoria");
             RuleFor(x => x.ColorHexa).NotEmpty().WithMessage("El Color es obligatorio");
             RuleFor(x => x.Activo).NotNull().WithMessage("El Color es obligatorio");
         }

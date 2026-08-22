6e77155e1df433b74503ae316a02a7e4a3323e2f
ricardoth <ricardotilleriaochoa@gmail.com>
2026-08-22 14:14:14 -0400

    fix cargo evento

--- ApiDecimatio/Validations/UpdateSectorDtoValidator.cs
+++ ApiDecimatio/Validations/UpdateSectorDtoValidator.cs
@@ -13,31 +13,6 @@
                 .MaximumLength(200)
                 .When(x => !string.IsNullOrEmpty(x.NombreSector));
 
-            RuleFor(x => x.CapacidadDisponible)
-                .GreaterThanOrEqualTo(0)
-                .When(x => x.CapacidadDisponible.HasValue);
-
-            RuleFor(x => x.CapacidadActual)
-                .GreaterThanOrEqualTo(0)
-                .When(x => x.CapacidadActual.HasValue);
-
-            RuleFor(x => x.CapacidadTotal)
-                .GreaterThanOrEqualTo(0)
-                .When(x => x.CapacidadTotal.HasValue);
-
-
-            RuleFor(x => x.Precio)
-                .GreaterThan(0)
-                .When(x => x.Precio.HasValue);
-
-            RuleFor(x => x.Cargo)
-                .GreaterThan(0)
-                .When(x => x.Cargo.HasValue);
-
-            RuleFor(x => x.Total)
-               .GreaterThan(0)
-               .When(x => x.Total.HasValue);
-
             RuleFor(x => x.ColorHexa)
                .MinimumLength(3)
                .MaximumLength(100)
